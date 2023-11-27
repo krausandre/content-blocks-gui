f13a5f97e778b42bcc36ec69b2103cb7e81cf34c
Jonas Eberle <flightvision@googlemail.com>
2023-11-27 17:23:00 +0100

    [TASK] render 2nd root component

--- packages/content_blocks_gui/Build/frontend/src/App.vue
+++ packages/content_blocks_gui/Build/frontend/src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <h1>{{store.getName}}</h1>
+  <EditCbView v-if="currentComponent === 'edit-cb-view'"/>
 </template>
 
 <script setup lang="ts">
+import {ref} from 'vue'
 import {useContentBlockStore} from './store/contentBlockStore'
+
+import EditCbView from './components/EditCbView.vue';
+
+const currentComponent = ref('edit-cb-view')
 
 const store = useContentBlockStore();
 </script>
---
+++ packages/content_blocks_gui/Build/frontend/src/components/EditCbView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+  <h1>Edit/Create CB</h1>
+
+  ...
+  </div>
+</template>
+<script setup lang="ts">
+</script>
