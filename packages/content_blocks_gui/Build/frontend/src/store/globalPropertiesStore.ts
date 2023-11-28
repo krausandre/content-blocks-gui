8f73b05b84da3358011f91ffb2510d2cf90cd68c
Philipp Kuhlmay <philipp.kuhlmay@wacon.de>
2023-11-28 11:59:59 +0100

    add global store

---
+++ packages/content_blocks_gui/Build/frontend/src/store/globalPropertiesStore.ts
@@ -0,0 +1,23 @@
+import {defineStore} from "pinia";
+
+export const useGlobalPropertiesStore = defineStore('globalProperties',{
+  state:() => ({
+    currentView: "c-b-list-view"
+  }),
+  getters: {
+    getCurrentView: state => state.currentView,
+    getIsCbListView: state => state.currentView === "c-b-list-view",
+    getIsEditView: state => state.currentView === "edit-cb-view",
+  },
+  actions: {
+    setCurrentView(newView: string) {
+        this.currentView = newView;
+    },
+    setCurrentViewToCbListView() {
+        this.setCurrentView("c-b-list-view");
+    },
+    setCurrentViewToEditView() {
+        this.setCurrentView("edit-cb-view");
+    }
+  },
+});
