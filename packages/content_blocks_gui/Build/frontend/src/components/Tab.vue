ba1b1443c99fe29b73ec8be4e6d549193a3492e2
Jonas Eberle <flightvision@googlemail.com>
2023-11-28 12:20:44 +0100

    [FEATURE] add tabs

---
+++ packages/content_blocks_gui/Build/frontend/src/components/Tab.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+const props = defineProps(['active']);
+</script>
+
+<template>
+  <div class="tab" :class="(active == 'true') ? 'active' : ''" ref="tabs">
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+.tab {
+  display: none;
+}
+
+.tab.active {
+  display: block;
+}
+</style>
--- packages/content_blocks_gui/Build/frontend/src/views/EditCB/PickerView.vue
+++ packages/content_blocks_gui/Build/frontend/src/views/EditCB/PickerView.vue
@@ -1,6 +1,39 @@
+<script setup lang="ts">
+import Tabs from '../../components/Tabs.vue'
+import Tab from '../../components/Tab.vue'
+</script>
+
 <template>
   <h1>Picker</h1>
+
+  <Tabs>
+    <Tab active="true" title="Settings">
+      <form>
+        <div class="mb-3">
+          <label for="xxx" class="form-label">Identifier</label>
+          <input class="form-control" id="xxx" aria-describedby="xxx">
+        </div>
+        <div class="mb-3">
+          <label for="xxx" class="form-label">Vendor</label>
+          <input class="form-control" id="xxx" aria-describedby="xxx">
+        </div>
+        <div class="mb-3">
+          <label for="xxx" class="form-label">Group</label>
+          Group Picker
+          <input class="form-control" id="xxx" aria-describedby="xxx">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </Tab>
+    <Tab title="Components">
+      @todo components
+    </Tab>
+    <Tab title="Basics">
+      @todo basics
+    </Tab>
+  </Tabs>
+
 </template>
 
-<script setup lang="ts">
-</script>
+<style scoped>
+</style>
