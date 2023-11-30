53e693ecf568ee7998328501e29d5411b3bdc24a
Jonas Eberle <flightvision@googlemail.com>
2023-11-30 18:19:38 +0100

    [TASK] adjust saveCb() for new structure

--- packages/content_blocks_gui/Build/frontend/src/Controller.ts
+++ packages/content_blocks_gui/Build/frontend/src/Controller.ts
@@ -1,31 +1,30 @@
 import {useContentBlockStore} from "./store/contentBlockStore";
 import {ContentBlock} from "@/models/ContentBlock";
+import axios from "axios";
 
 export class Controller {
   saveCb() {
-    console.log('saving', useContentBlockStore().contentBlock)
-    const data = ContentBlock.asJson(useContentBlockStore().contentBlock)
-    fetch(
+    const cb = useContentBlockStore().contentBlock;
+
+    // @todo
+    axios.postForm(
       TYPO3.settings.ajaxUrls.content_blocks_gui_save_content_type,
       {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {'Content-Type': 'application/json'},
+        contentType: ContentBlock.contentType(cb),
+        contentBlock: ContentBlock.asJson(cb),
+        extension: 'samples',
+        // @todo mode: 'create' | 'copy' | 'edit';
+        mode: 'edit',
       }
-    )
-      .then(
-        (response) => response.json()
-      )
-      .then(
-        (data) => {
-          console.log('Success:', data);
-        }
-      )
-      .catch(
-        (error) => {
-          console.error('Error:', error);
-        }
-      );
+    ).then(
+      response => {
+        console.log(response.data);
+      }
+    ).catch(
+      error => {
+        console.error('Error:', error);
+      }
+    );
   }
 }
 
