bcd16cd968826ceb3bfb04f8bf9865ab315d1558
Manuel Schnabel <mschnabel1992@gmail.com>
2023-11-30 11:57:42 +0100

    [TASK] Refactor downloadCb action

--- packages/content_blocks_gui/Resources/Public/JavaScript/ajax-request-test.js
+++ packages/content_blocks_gui/Resources/Public/JavaScript/ajax-request-test.js
@@ -14,7 +14,7 @@ class AjaxRequestTest {
     });
   }
   sendAjaxRequest() {
-    return new AjaxRequest(TYPO3.settings.ajaxUrls['content_blocks_gui_save_content_type'])
+    return new AjaxRequest(TYPO3.settings.ajaxUrls['content_blocks_gui_download_cb'])
       .post(
         /*
         // save content block with content type "Content Element"
@@ -91,6 +91,8 @@ class AjaxRequestTest {
           }
         }
          */
+        /*
+        // copy content block with content type "Content Element"
         {
           extension: "samples_copy",
           contentType: "content-element",
@@ -116,12 +118,11 @@ class AjaxRequestTest {
             ]),
           }
         }
-        // delete content block
-        /*
+         */
+        // delete/download content block
         {
           name: "test-123/test-12",
         }
-         */
       )
       .then(async function (response) {
         const resolved = await response.resolve();
