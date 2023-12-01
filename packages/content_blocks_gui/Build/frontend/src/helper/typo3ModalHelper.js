4cbb7b3dde765bc8923763b9e0630603ca3aec97
Jonas Eberle <flightvision@googlemail.com>
2023-12-01 12:17:12 +0100

    [TASK] lint

--- packages/content_blocks_gui/Build/frontend/src/helper/typo3ModalHelper.js
+++ packages/content_blocks_gui/Build/frontend/src/helper/typo3ModalHelper.js
@@ -1,76 +1,75 @@
 import Modal from '@typo3/backend/modal.js';
 
-
 export const shootConfirmModal = (title, message) => {
-    Modal.confirm(
-        title,
-        message,
-        0, [
-            {
-                text: 'OK!',
-                trigger: function() {
-                    top.TYPO3.Modal.dismiss();
-                }
-            }
-        ]
-    );
+  Modal.confirm(
+    title,
+    message,
+    0, [
+      {
+        text: 'OK!',
+        trigger: function () {
+          top.TYPO3.Modal.dismiss();
+        }
+      }
+    ]
+  );
 }
 
 export const shootWarningModal = (title, message) => {
-    Modal.confirm(
-        title,
-        message,
-        1,
-        [
-            {
-                text: 'OK!',
-                trigger: function() {
-                    top.TYPO3.Modal.dismiss();
-                }
-            }
-        ],
-    )
+  Modal.confirm(
+    title,
+    message,
+    1,
+    [
+      {
+        text: 'OK!',
+        trigger: function () {
+          top.TYPO3.Modal.dismiss();
+        }
+      }
+    ],
+  )
 }
 
 export const shootDangerModal = (
+  title,
+  message,
+  okayFunction = () => top.TYPO3.Modal.dismiss()
+) => {
+  Modal.confirm(
     title,
     message,
-    okayFunction = () => top.TYPO3.Modal.dismiss()
-) => {
-    Modal.confirm(
-        title,
-        message,
-        2,
-        [
-            {
-                text: "Cancel",
-                trigger: function() {
-                  top.TYPO3.Modal.dismiss();
-                }
-            },
-            {
-                text: 'Okay',
-                btnClass:"btn-danger",
-                trigger: function() {
-                  okayFunction();
-                  top.TYPO3.Modal.dismiss();
-                }
-            }
-        ],
-    )
+    2,
+    [
+      {
+        text: "Cancel",
+        trigger: function () {
+          top.TYPO3.Modal.dismiss();
+        }
+      },
+      {
+        text: 'Okay',
+        btnClass: "btn-danger",
+        trigger: function () {
+          okayFunction();
+          top.TYPO3.Modal.dismiss();
+        }
+      }
+    ],
+  )
 }
 export const shootPrimaryModal = (title, message) => {
-    Modal.confirm(
-        title,
-        message,
-        3,
-        [
-            {
-                text: 'OK!',
-                trigger: function() {
-                    top.TYPO3.Modal.dismiss();
-                }
-            }
-        ],
-    )
+  Modal.confirm(
+    title,
+    message,
+    3,
+    [
+      {
+        text: 'OK!',
+        trigger: function () {
+          top.TYPO3.Modal.dismiss();
+        }
+      }
+    ],
+  )
 }
--- packages/content_blocks_gui/Build/frontend/src/helper/typo3NotificationHelper.js
+++ packages/content_blocks_gui/Build/frontend/src/helper/typo3NotificationHelper.js
@@ -2,22 +2,22 @@ import Notification from '@typo3/backend/notification.js';
 
 
 export const shootNoticeNotification = (title, message, duration = 5) => {
-    Notification.notice(title, message, duration);
+  Notification.notice(title, message, duration);
 }
 
 export const shootInfoNotification = (title, message, duration = 5) => {
-    Notification.info(title, message, duration);
+  Notification.info(title, message, duration);
 }
 
 export const shootSuccessNotification = (title, message, duration = 5) => {
-    Notification.success(title, message, duration);
+  Notification.success(title, message, duration);
 }
 
 export const shootWarningNotification = (title, message, duration = 5) => {
-    Notification.warning(title, message, duration);
+  Notification.warning(title, message, duration);
 }
 
 export const shootErrorNotification = (title, message, duration = 5) => {
-    Notification.error(title, message, duration);
+  Notification.error(title, message, duration);
 }
 
