b2f1e348c2e7b25810b50c3550ae95a83f5a4e7e
Philipp Kuhlmay <info@treupo.de>
2024-06-07 12:32:17 +0200

    Implement FormKit in EditCB/LeftPaneSettings.vue and add CSS for FormKit help
    The standard <form> has been replaced with the FormKit component in EditCB/LeftPaneSettings.vue for a more consistent and organized form layout. In addition, the main.css file has been updated to include specific styling for the FormKit help information. This enhances the visual representation of the form and its associated help text.

--- packages/content_blocks_gui/Build/frontend/src/components/EditCB/LeftPaneSettings.vue
+++ packages/content_blocks_gui/Build/frontend/src/components/EditCB/LeftPaneSettings.vue
@@ -1,104 +1,107 @@
 <template>
-  <form>
-    <div class="mb-3">
-      <label for="hostExtension" class="form-label">Extension</label>
-      <select
-        class="form-control"
-        id="hostExtension"
-        aria-describedby="hostExtension"
-        v-model="contentBlockStore.contentBlock.hostExtension"
+  <FormKit
+    type="form"
+    class="mb-3"
+    :actions="false"
+    @submit="console.log('test')"
+  >
+    <FormKit
+      label="Extension"
+      type="select"
+      name="hostExtension"
+      :classes="{label: 'form-label', input: 'form-control', outer: 'mb-3'}"
+      v-model="contentBlockStore.contentBlock.hostExtension"
+    >
+      <option
+        v-for="item in extensionListStore.list"
+        :value="item.extension"
+        :selected="item.extension === contentBlockStore.contentBlock.hostExtension"
       >
-        <option
-          v-for="item in extensionListStore.list"
-          :value="item.extension"
-          :selected="item.extension === contentBlockStore.contentBlock.hostExtension"
-        >
-          {{ item.extension }} [{{ item.vendor }}/{{ item.package }}]
-        </option>
-      </select>
-    </div>
-    <div class="mb-3">
-      <label for="name" class="form-label">Name</label>
-      <input class="form-control" id="name" aria-describedby="name"
-        v-model="contentBlockStore.getContentBlock.name"
+        {{ item.extension }} [{{ item.vendor }}/{{ item.package }}]
+      </option>
+    </FormKit>
+    <FormKit
+      label="Name"
+      name="name"
+      :classes="{label: 'form-label', input: 'form-control', outer: 'mb-3'}"
+      v-model="contentBlockStore.getContentBlock.name"
+      help="The name of the content-block. Must contain a vendor and a name. <code>vendor/name</code>"
+    />
+    <FormKit
+      label="Title"
+      name="title"
+      :classes="{label: 'form-label', input: 'form-control', outer: 'mb-3'}"
+      v-model="contentBlockStore.getContentBlock.yaml.title"
+      help="This is the title of the Content Block. This could contain any string with spaces."
+    />
+    <FormKit
+      type="checkbox"
+      label="Prefix fields?"
+      name="prefixFields"
+      :classes="{outer: 'form-check mb-3', label: 'form-check-label', input: 'form-check-input'}"
+      v-model="contentBlockStore.getContentBlock.yaml.prefixFields"
+    />
+    <FormKit
+      type="select"
+      label="Prefix type"
+      name="prefixType"
+      :classes="{label: 'form-label', input: 'form-control', outer: 'mb-3'}"
+      v-model="contentBlockStore.getContentBlock.yaml.prefixType"
       >
-      <div id="nameHelp" class="form-text">The name of the content-block. Must contain a vendor and a name. <code>vendor/name</code></div>
-    </div>
-    <div class="mb-3">
-      <label for="title" class="form-label">Title</label>
-      <input class="form-control" id="title" aria-describedby="title" v-model="contentBlockStore.getContentBlock.yaml.title"
+      <option
+        v-for="item in PrefixTypes"
+        :value="item"
+        :selected="item === contentBlockStore.contentBlock.yaml.prefixType"
       >
-      <div id="titleHelp" class="form-text">This is the title of the Content Block. This could contain any string with spaces.</div>
-    </div>
-    <div class="form-check mb-3">
-      <input class="form-check-input" type="checkbox" v-model="contentBlockStore.getContentBlock.yaml.prefixFields" id="prefixFields">
-      <label class="form-check-label" for="prefixFields">
-        Prefix fields?
-      </label>
-    </div>
-    <div class="mb-3">
-      <label class="form-label" for="prefixType">Prefix type</label>
-      <select
-        class="form-control"
-        id="prefixType"
-        v-model="contentBlockStore.contentBlock.yaml.prefixType"
-        aria-describedby="prefixType">
-        <option
-          v-for="item in PrefixTypes"
-          :value="item"
-          :selected="item === contentBlockStore.contentBlock.yaml.prefixType"
-        >
-          {{ item }}
-        </option>
-      </select>
-    </div>
-    <div class="mb-3">
-      <label for="vendorPrefix" class="form-label">Vendor prefix</label>
-      <input class="form-control" id="vendorPrefix" aria-describedby="vendor-prefix"
-             v-model="contentBlockStore.getContentBlock.yaml.vendorPrefix"
+        {{ item }}
+      </option>
+    </FormKit>
+    <FormKit
+      label="Vendor prefix"
+      name="vendorPrefix"
+      :classes="{label: 'form-label', input: 'form-control', outer: 'mb-3'}"
+      v-model="contentBlockStore.getContentBlock.yaml.vendorPrefix"
+      help="If set, this prefix will be used instead of the vendor part of <code>name</code>"
+    />
+    <FormKit
+      label="Priority"
+      name="priority"
+      type="number"
+      :classes="{label: 'form-label', input: 'form-control', outer: 'mb-3'}"
+      v-model="contentBlockStore.getContentBlock.yaml.priority"
+      help="The priority can be used to prioritize certain Content Blocks in the loading order. Higher priorities will be loaded before lower ones."
+    />
+    <FormKit
+      label="Group"
+      type="select"
+      name="group"
+      :classes="{label: 'form-label', input: 'form-control', outer: 'mb-3'}"
+      v-model="contentBlockStore.getContentBlock.yaml.group"
+      aria-describedby="group"
+    >
+      <option
+        v-for="(item, key) in groupListStore.list"
+        :value="key"
+        :key="{ key }"
+        :selected="key === contentBlockStore.contentBlock.yaml.group"
       >
-      <div id="titleHelp" class="form-text">If set, this prefix will be used instead of the vendor part of <code>name</code></div>
-    </div>
-    <div class="mb-3">
-      <label for="priority" class="form-label">Priority</label>
-      <input class="form-control" id="priority" aria-describedby="priority" type="number"
-             v-model="contentBlockStore.getContentBlock.yaml.priority"
-      >
-      <div id="priorityHelp" class="form-text">The priority can be used to prioritize certain Content Blocks in the loading order. Higher priorities will be loaded before lower ones.</div>
-    </div>
-    <div class="mb-3" v-if="ContentBlock.isContentElement(contentBlockStore.contentBlock) /* @todo */">
-      <label for="group" class="form-label">Group -> nur für content elements</label>
-      <select
-        class="form-control"
-        id="group"
-        v-model="contentBlockStore.contentBlock.yaml.group"
-        aria-describedby="group">
-        <option
-          v-for="(item, key) in groupListStore.list"
-          :value="key"
-          :key="{ key }"
-          :selected="key === contentBlockStore.contentBlock.yaml.group"
-        >
-          {{ item }} [{{ key }}]
-        </option>
-      </select>
-    </div>
-    <div class="mb-3">
-      <pre>
-        {{ JSON.stringify(contentBlockStore.getContentBlock, null, 2) }}
-      </pre>
-      <pre>
-        {{ JSON.stringify(contentBlockStore.getContentBlock.getFields, null, 2) }}
-      </pre>
-    </div>
-<!--    Is no root element
-<div class="mb-3">
-      <label for="table" class="form-label">Table</label>
-      <input class="form-control" id="table" aria-describedby="table"
-             :value="contentBlockStore.getContentBlock.yaml.table"
-      >
-    </div>-->
-  </form>
+        {{ item }} [{{ key }}]
+      </option>
+    </FormKit>
+    <FormKit
+      type="submit"
+      label="Save"
+      :classes="{input: 'btn btn-primary', outer: 'mb-3'}"
+    />
+  </FormKit>
+  <div class="mb-3">
+    <pre>
+      {{ JSON.stringify(contentBlockStore.getContentBlock, null, 2) }}
+    </pre>
+    <pre>
+      {{ JSON.stringify(contentBlockStore.getContentBlock.getFields, null, 2) }}
+    </pre>
+  </div>
 </template>
 
 <script setup>
@@ -107,6 +110,7 @@ import {useContentBlockStore} from "@/store/contentBlockStore";
 import {useExtensionListStore} from "@/store/extensionListStore";
 import {useGroupListStore} from "@/store/groupListStore";
 import {computed, watchEffect} from "vue";
+import {FormKit} from "@formkit/vue";
 
 const contentBlockStore = useContentBlockStore();
 const extensionListStore = useExtensionListStore();
